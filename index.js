dc09efa2bc83a8ffbcfc510254f04b8cb25b0afc
lucas <mrlucaszarate@gmail.com>
2022-12-20 14:55:38 -0300

    Mostar texto encriptado/desencriptado y funcion de copiar

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const btnEncriptar= document.getElementById("encriptar")
 const btnDesencriptar= document.getElementById("desencriptar")
 const textInput= document.getElementById("text-input")
+const textOutputContainer= document.querySelector(".textOutput")
+const textOutputCrypContainer= document.querySelector(".textOutputCrypted")
+const textCrypted=document.querySelector(".textCrypted")
+const copyBtn=document.querySelector(".buttonCrypted")
+
 
 const libro={
     a:"ai",
@@ -13,26 +18,31 @@ const libro={
 const encriptar=(value)=>{
     
 const arr=value.split("")
-return  arr.reduce((acc,val)=>{
-    libro[val]? acc.concat(libro[val]):acc.concat(val)
-},"")
-
-//console.log(cifrado)
-
+return  arr.reduce((acc,val)=>libro[val]?acc.concat(libro[val]):acc.concat(val),"")
 }
 
 const desencriptar=(value)=>{
     return Object.keys(libro)
                 .reduce((acc,val)=>acc.replaceAll(libro[val],val),value)
-
-   // console.log(textoPlano)
-    
+ 
 }
 
 btnEncriptar.addEventListener("click",()=>{
-    encriptar(textInput.value)
+    const text=encriptar(textInput.value)
+    textCrypted.innerHTML=(text)
+     textOutputContainer.classList.toggle("empty")
+     textOutputCrypContainer.classList.toggle("empty")
 })
 
 btnDesencriptar.addEventListener("click",()=>{
-    desencriptar(textInput.value)
+   const text= desencriptar(textInput.value)
+   textCrypted.innerHTML=(text)
+
+})
+
+copyBtn.addEventListener("click",()=>{
+    const text=textCrypted.textContent
+    navigator.clipboard.writeText(text);
+    textInput.value=(text)
+
 })
